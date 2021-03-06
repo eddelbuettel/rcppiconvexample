ef295900e684857021ff1de203fed031350f915f
Dirk Eddelbuettel <edd@debian.org>
2021-03-06 14:26:12 -0600

    updated test

--- inst/tinytest/test_read.R
+++ inst/tinytest/test_read.R
@@ -1,11 +1,9 @@
 
 onWindows <- .Platform$OS.type == "windows"
 
-#txt <- demo_read_file(system.file("rawdata", "qc.txt", package="rcppstringtest"))
+win1252file <- system.file("rawdata", "windows-1252.txt", package="rcppstringtest")
+win1252txt <- demo_read_file_enc(win1252file, "windows-1252")
+utf8file <- system.file("rawdata", "utf8.txt", package="rcppstringtest")
+utf8txt <- demo_read_file_enc(utf8file, "UTF-8")
 
-#if (!onWindows) expect_true(identical(txt, "Québec\n"))
-#if (onWindows) expect_false(identical(txt, "Québec\n"))
-#if (onWindows) expect_error(stopifnot(txt == "Québec\n"))
-#if (onWindows) expect_false(txt == "Québec\n")
-
-#expect_equal(txt, "Québec\n")
+expect_equal(substr(win1252txt, 1, 62), substr(utf8txt, 1, 62))
